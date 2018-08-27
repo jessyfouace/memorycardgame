70a7f3a375f397297795262d988976c832b2b78e
Jessy Fouace <jessy.fouace62@gmail.com>
2018-08-27 11:55:03 +0200

    With playing option and loose + win

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ allcards.style.display="none";
 startgame.onclick = function() {gameStart("start")};
 restartgame.style.display="none";
 restartgame.onclick = function() {gameStart("restart")};
-i = 41;
+i = 51;
 var tabImg = ["url('img/carte1.png')", "url('img/carte1.png')", "url('img/carte2.png')", "url('img/carte2.png')", "url('img/carte3.png')", "url('img/carte3.png')", "url('img/carte4.png')", "url('img/carte4.png')", "url('img/carte5.png')", "url('img/carte5.png')", "url('img/carte6.png')", "url('img/carte6.png')", "url('img/carte7.png')", "url('img/carte7.png')", "url('img/carte8.png')", "url('img/carte8.png')"];
 var TabImgRandom;
 var newtable = [];
@@ -29,7 +29,7 @@ function gameStart(e) {
     textbuttonstart.classList.add("col-6");
     textbuttonstart.classList.remove("mx-auto");
     timerstart.value = "Time left " + i + "s";
-  } else {
+  } else if (i == 0) {
     allcards.style.display="none";
     textbuttonstart.classList.remove("col-6");
     restartgame.style.display="inline-block";
@@ -58,7 +58,7 @@ function gameStart(e) {
   if (vale == "restart") {
     restartgame.style.display="none";
     changetext.innerHTML = "Click on the cards and try to find the pairs"
-    i = 41;
+    i = 51;
     vale = "";
     clearTimeout(startgamevar);
     for (var e = 0; e < 16 ; e++) {
@@ -82,21 +82,57 @@ function gameStart(e) {
   return vale;
 }
 
+var safeguardfirstclick;
+var clickmyid;
 var vie = 0;
+var tablforcompare = [];
+var changechildren;
+var changechildren2;
+var search = 0;
+
 function getId(monId) {
   vie++;
-  if (vie < 3) {
-    var clickmyid = monId;
+
+  if (vie == 1) {
+    safeguardfirstclick = monId;
+    var changeopa = document.getElementById(safeguardfirstclick);
+    var children = changeopa.children[0];
+    changechildren = children;
+    changechildren.style.opacity="0";
+    var fortablcompare = changeopa.style.backgroundImage;
+    tablforcompare.push(fortablcompare);
+  }
+  if (vie == 2) {
+    clickmyid = monId;
     var stylebyid = document.getElementById(clickmyid);
-    stylebyid.style.opacity="0";
-    if (stylebyid.parentElement.backgroundImage == stylebyid.parentElement.backgroundImage) {
-      console.log("gregerge");
-      vie = 0;
-      return vie;
-    } else {
-      vie = 0;
-      stylebyid.style.opacity="1";
-      return vie;
-    }
+    var children2 = stylebyid.children[0];
+    changechildren2 = children2;
+    changechildren2.style.opacity="0";
+    var test = stylebyid.style.backgroundImage;
+    tablforcompare.push(test);
+    setTimeout(compare, 1000);
+  }
+}
+
+function compare() {
+  if (tablforcompare[0] == tablforcompare[1]) {
+    vie = 0;
+    tablforcompare = [];
+    search++;
+  } else {
+    vie = 0;
+    tablforcompare = [];
+    changechildren2.style.opacity="1";
+    changechildren.style.opacity="1";
+  }
+  if (search == 8) {
+    vie = 0;
+    i = "You win";
+    allcards.style.display="none";
+    textbuttonstart.classList.remove("col-6");
+    restartgame.style.display="inline-block";
+    changetext.innerHTML = "Click on restart for restart a game"
+    timerstart.value = "You Win, Congratulations";
+    return i;
   }
 }
